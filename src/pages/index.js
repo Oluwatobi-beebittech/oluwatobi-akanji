b442129fc2ad3f15558c41f2d8ca7c19621d6b52
Oluwatobi-beebittech <akanjioluwatobishadrach@yahoo.com>
2021-07-17 13:53:41 +0100

    edit height

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ import "../styles/site.css";
 // markup
 const IndexPage = () => {
   return (
-    <main className="">
+    <main className="h-screen">
       <title>Oluwatobi Akanji</title>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
