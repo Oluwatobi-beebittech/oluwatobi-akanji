75d389999c374649712e9e655a204c25d14bf9e4
Oluwatobi-beebittech <akanjioluwatobishadrach@yahoo.com>
2021-07-21 00:27:32 +0100

    about page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -91,7 +91,7 @@ const AboutPage = () => {
               </div>
               <div className="flex ml-5 space-x-4 mt-1">
                 <span
-                  className="iconify text-7xl md:text-2xl text-purple-900"
+                  className="iconify text-5xl md:text-2xl text-purple-900"
                   data-icon="ci:user-check"
                 ></span>
                 <p className="md:w-1/2">
@@ -101,33 +101,62 @@ const AboutPage = () => {
               </div>
               <div className="flex ml-5 space-x-4 mt-1">
                 <span
-                  className="iconify text-7xl md:text-2xl text-purple-900"
+                  className="iconify text-6xl md:text-2xl text-purple-900"
                   data-icon="ci:user-check"
                 ></span>
                 <p className="md:w-1/2">
-                  Focused on implementing highly performant secure and scalable systems.
+                  Focused on implementing highly performant, secure, and
+                  scalable systems.
                 </p>
               </div>
               <div className="flex ml-5 space-x-4 mt-1">
                 <span
-                  className="iconify text-7xl md:text-2xl text-purple-900"
+                  className="iconify text-5xl md:text-2xl text-purple-900"
                   data-icon="ci:user-check"
                 ></span>
                 <p className="md:w-1/2">
-                  Currently working as an IT Infrastructure Engineer at <a href="https://steamledge.com" className="font-bold text-purple-900">Steamledge</a>.
+                  Currently working as an IT Infrastructure Engineer at{" "}
+                  <a
+                    href="https://steamledge.com"
+                    className="font-bold text-purple-900"
+                  >
+                    Steamledge
+                  </a>
+                  .
                 </p>
               </div>
 
               <h4 className="mt-10 font-bold">What I do</h4>
-              <div className="flex ml-5 space-x-4 mt-1 ">
-                <span
-                  className="iconify text-7xl md:text-2xl text-purple-900"
-                  data-icon="ci:user-check"
-                ></span>
-                <p className="md:w-1/2">
-                  A <b>Cloud Software Engineer</b> with experience developing
-                  websites, full-stack web applications, APIs.
-                </p>
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-5 md:mt-2">
+                <div className="text-center rounded-lg shadow-box h-40 transition transform ease-in-out hover:scale-105">
+                  <span
+                    className="iconify mx-auto my-2 text-5xl text-red-500"
+                    data-icon="ant-design:cloud-twotone"
+                  ></span>
+                  <h4 className="font-bold">Cloud Architecture</h4>
+                  <p>
+                    I design the architecture of software to run on the cloud.
+                  </p>
+                </div>
+                <div className="text-center rounded-lg shadow-box h-40 transition transform ease-in-out hover:scale-105">
+                  <span
+                    className="iconify mx-auto my-2 text-5xl text-blue-500"
+                    data-icon="ant-design:api-twotone"
+                  ></span>
+                  <h4 className="font-bold">Back-End Web Development</h4>
+                  <p>
+                    I implement the logic of web applications as APIs using
+                    codes.
+                  </p>
+                </div>
+                <div className="text-center rounded-lg shadow-box h-40 transition transform ease-in-out hover:scale-105">
+                  <span
+                    className="iconify mx-auto my-2 text-5xl text-green-500"
+                    data-icon="icon-park-outline:cycle-arrow"
+                  ></span>
+                  <h4 className="font-bold">Devops</h4>
+                  <p>I configure and manage cloud resources for CI/CD.</p>
+                </div>
               </div>
             </div>
           </div>
