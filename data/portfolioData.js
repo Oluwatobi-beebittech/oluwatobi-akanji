a1b6fbb296331566e563ca2a6561f9497e86fb1a
Oluwatobi-beebittech <akanjioluwatobishadrach@yahoo.com>
2021-07-26 10:14:25 +0100

    added cloudinary img link

--- data/portfolioData.js
+++ data/portfolioData.js
@@ -1,17 +1,15 @@
-import WebBuddy from "../src/images/webbuddy.png";
-import PersonalWeb from "../src/images/oluwatobiakanji.png";
 
 export const portfolios = [
   {
     title: "Personal Website",
     slug: "/portfolio/oluwatobiakanji/",
-    image: PersonalWeb,
+    image: "https://res.cloudinary.com/dfybu7w8o/image/upload/v1627289342/oluwatobiakanji.com/oluwatobiakanji_hbcjfs.png",
     techStacks: ["Gatsby JS", "HTML & CSS", "Tailwind CSS"],
   },
   {
     title: "WebBuddy",
     slug: "/portfolio/webbuddy/",
-    image: WebBuddy,
+    image: "https://res.cloudinary.com/dfybu7w8o/image/upload/v1627289339/oluwatobiakanji.com/webbuddy_vrx7xt.png",
     techStacks: ["React JS", "HTML & CSS", "Bootstrap"],
   },
 ];
