415e7c8e97096e0d540eaf013443a34869592eab
Oluwatobi-beebittech <akanjioluwatobishadrach@yahoo.com>
2021-07-30 12:50:49 +0100

    added phallery

--- data/portfolioData.js
+++ data/portfolioData.js
@@ -1,6 +1,12 @@
 
 export const portfolios = [
   {
+    title: "Phallery",
+    slug: "/portfolio/phallery/",
+    image: "https://res.cloudinary.com/dfybu7w8o/image/upload/v1627642969/oluwatobiakanji.com/phallery_zkjwqr.png",
+    techStacks: ["Laravel","React JS", "HTML & CSS", "Bootstrap"],
+  },
+  {
     title: "Personal Website",
     slug: "/portfolio/oluwatobiakanji/",
     image: "https://res.cloudinary.com/dfybu7w8o/image/upload/v1627289342/oluwatobiakanji.com/oluwatobiakanji_hbcjfs.png",
