b7c93055df9fbd1813f8f74a52af6cecf0c031c2
Oluwatobi-beebittech <akanjioluwatobishadrach@yahoo.com>
2021-09-23 10:51:20 +0100

    worked on charts crypto

--- data/portfolioData.js
+++ data/portfolioData.js
@@ -1,21 +1,36 @@
-
 export const portfolios = [
+  {
+    title: "ChartsCrypto",
+    slug: "/portfolio/chartscrypto/",
+    image:
+      "https://res.cloudinary.com/dfybu7w8o/image/upload/v1632353358/oluwatobiakanji.com/crypto_fqt5g8.png",
+    techStacks: [
+      "React JS",
+      "HighCharts JS",
+      "HTML & CSS",
+      "Tailwind CSS",
+      "Messari API",
+    ],
+  },
   {
     title: "Phallery",
     slug: "/portfolio/phallery/",
-    image: "https://res.cloudinary.com/dfybu7w8o/image/upload/v1627642969/oluwatobiakanji.com/phallery_zkjwqr.png",
-    techStacks: ["Laravel","React JS", "HTML & CSS", "Bootstrap"],
+    image:
+      "https://res.cloudinary.com/dfybu7w8o/image/upload/v1627642969/oluwatobiakanji.com/phallery_zkjwqr.png",
+    techStacks: ["Laravel", "React JS", "HTML & CSS", "Bootstrap"],
   },
   {
     title: "Personal Website",
     slug: "/portfolio/oluwatobiakanji/",
-    image: "https://res.cloudinary.com/dfybu7w8o/image/upload/v1627289342/oluwatobiakanji.com/oluwatobiakanji_hbcjfs.png",
+    image:
+      "https://res.cloudinary.com/dfybu7w8o/image/upload/v1627289342/oluwatobiakanji.com/oluwatobiakanji_hbcjfs.png",
     techStacks: ["Gatsby JS", "HTML & CSS", "Tailwind CSS"],
   },
   {
     title: "WebBuddy",
     slug: "/portfolio/webbuddy/",
-    image: "https://res.cloudinary.com/dfybu7w8o/image/upload/v1627289339/oluwatobiakanji.com/webbuddy_vrx7xt.png",
+    image:
+      "https://res.cloudinary.com/dfybu7w8o/image/upload/v1627289339/oluwatobiakanji.com/webbuddy_vrx7xt.png",
     techStacks: ["React JS", "HTML & CSS", "Bootstrap"],
   },
 ];
